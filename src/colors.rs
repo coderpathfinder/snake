6848f799a4799d9784e613d83c0e60dd1458008d
pathfinder <pathfinder8848@163.com>
2020-03-07 19:38:20 +0800

    yee

--- src/colors.rs
+++ src/colors.rs
@@ -3,6 +3,6 @@ use piston_window::types::Color;
 pub const BACKGROUND: Color = [0.0, 0.0, 0.0, 1.0];//背景黑色
 pub const SCORE: Color = [1.0, 1.0, 1.0, 1.0];//分数白色
 pub const SNAKE: Color = [0.0, 1.0, 0.0, 1.0];//蛇绿色
-pub const EYE: Color = [0.5, 0.0, 0.5, 1.0];//眼睛紫色
+pub const EYE: Color = [1.0, 1.0, 0.0, 1.0];//眼睛黄色
 pub const FOOD: Color = [1.0, 0.0, 0.0, 1.0];//食物红色
 pub const OVERLAY: Color = [0.0, 0.0, 1.0, 0.5];//失败半透明蓝色
