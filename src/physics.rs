7f1840c56fda490edf6ef102258117bc8880131d
pathfinder <pathfinder8848@163.com>
2020-03-07 18:52:50 +0800

    zxs

--- src/physics.rs
+++ src/physics.rs
@@ -7,7 +7,10 @@ pub struct Position{
 impl Position {
     pub fn move_to_dir(&mut self, dir: Direction){
         match dir {
-
+            Direction::Up => self.y -= 1,
+            Direction::Down => self.y += 1,
+            Direction::Left => self.x -= 1,
+            Direction::Right => self.x += 1
         }
     }
 }
